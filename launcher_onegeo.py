11cef5e1ce3bd49a91723da4f40fe274e5064d0c
Rachid Moyse <rachidmoyse1@hotmail.com>
2019-10-03 05:56:33 -0500

    Poniendo el launcher a ejecutar solamente el .sh

--- launcher_onegeo.py
+++ launcher_onegeo.py
@@ -7,15 +7,9 @@ import pdb
 
 def main():
     # Importando archivos para que se ejecute el programa
-    # Importando main_server
-    from dependencias.main_server import main
-
-    # Corriendo la funcion principal de main_server
-    main()
-
     time.sleep(0.9)
     # pdb.set_trace()
-    os.system("./dependencias/start.sh")
+    os.system("sudo /bin/bash ./dependencias/start.sh")
 
 
 if __name__ == "__main__":
